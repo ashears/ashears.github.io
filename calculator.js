73aad09cab6fc93debea224517bba2be384b563d
Ashton Shears <shears.ash@gmail.com>
2020-05-03 14:29:54 -0700

    fix calc

--- calculator.js
+++ calculator.js
@@ -1,7 +1,7 @@
 function calculate() {
-  var ot = document.getElementById('Optimistic time').value;
-  var rt = document.getElementById('Realistic time').value;
-  var pt = document.getElementById('Pessimistic time').value;
+  var ot = document.getElementById('optimistic_time').value;
+  var rt = document.getElementById('realistic_time').value;
+  var pt = document.getElementById('pessimistic_time').value;
   var result = document.getElementById('result');
   var myResult = (+ot + +rt*4 + +pt)/6;
   result.value = myResult;
