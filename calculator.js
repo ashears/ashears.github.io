a2a04fe9ae19969d9ceabb29840d0bcb797450cc
Ashton Shears <shears.ash@gmail.com>
2020-12-17 07:53:21 -0800

    clean up

--- calculator.js
+++
@@ -1,8 +0,0 @@
-function calculate() {
-  var ot = document.getElementById('optimistic_time').value;
-  var rt = document.getElementById('realistic_time').value;
-  var pt = document.getElementById('pessimistic_time').value;
-  var result = document.getElementById('result');
-  var myResult = (+ot + +rt*4 + +pt)/6;
-  result.value = myResult;
-}
